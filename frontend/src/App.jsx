046f91c52d28a6d401a9757618ab93d56cb54958
Victor Valentim <victorvalentim0803@gmail.com>
2026-02-10 00:04:22 -0300

    feat: setup inicial das estruturas de frontend e backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white shadow-lg p-5 bg-black rounded-xl">
+        Tailwind funcionando! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App  
